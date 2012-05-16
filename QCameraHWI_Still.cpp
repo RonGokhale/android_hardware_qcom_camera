5f67e29720102d0a74995549c013da09a85de52b
Ashwini Rao <ashwinik@codeaurora.org>
2012-05-15 18:02:49 -0700

    Camera: Fix deadlock in receiveCompleteJpegPicture on Cancel Picture
    (cherry picked from commit 9a68e261960aeaf296d63d5e4dff08c7d692a688)
    
    (cherry picked from commit 843aed5e296fe53be775effbb25af650a38f7408)
    
    Change-Id: I7fd615d4a0bcb586e6cfe595e02369b738c658b9

--- QCameraHWI_Still.cpp
+++ QCameraHWI_Still.cpp
@@ -256,12 +256,12 @@ receiveCompleteJpegPicture(jpeg_event_t event)
     camera_data_callback jpg_data_cb = NULL;
     bool fail_cb_flag = false;
 
-    mStopCallbackLock.lock( );
     if(!mActive && !isLiveSnapshot()) {
         ALOGE("%s : Cancel Picture",__func__);
         fail_cb_flag = true;
         goto end;
     }
+    mStopCallbackLock.lock( );
 
     if(mCurrentFrameEncoded!=NULL /*&& !isLiveSnapshot()*/){
         ALOGV("<DEBUG>: Calling buf done for snapshot buffer");
