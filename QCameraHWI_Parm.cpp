a9b81090a0bec7418d2a4122815247697e9188b4
Rajaram Gudivada <rajaramg@codeaurora.org>
2012-11-14 12:52:42 +0530

    Camera: Change ION_CP_MM_HEAP to ION_CAMERA_HEAP
    This change is required Since ION_CP_MM_HEAP_ID(Content protection heap)
    is defined only for A-family, 8660 and higher end targets and same heap is
    not there for other targets like 8x25 & 7x30/8x55.
    
    Also Cleapup the code i.e, replace whereever ION_CP_MM_HEAP_ID
    with corresponding local flag(CAMERA_ION_HEAP_ID)
    
    Change-Id: I6df861826ac32b228185ae97a8445ac04032e8d0
    CRs-fixed: 411906

--- QCameraHWI_Parm.cpp
+++ QCameraHWI_Parm.cpp
@@ -3792,7 +3792,7 @@ status_t QCameraHardwareInterface::setHistogram(int histogram_en)
                 }
                 mHistServer.size = sizeof(camera_preview_histogram_info);
 #ifdef USE_ION
-                if(allocate_ion_memory(&mHistServer, cnt, ION_CP_MM_HEAP_ID) < 0) {
+                if(allocate_ion_memory(&mHistServer, cnt, ION_CAMERA_HEAP_ID) < 0) {
                   ALOGE("%s ION alloc failed\n", __func__);
                   return -1;
                 }
