48b4db13330f66107c97b22e11a701f2647d205f
Roja Rani Yarubandi <rojay@codeaurora.org>
2012-12-30 16:04:05 +0530

    camera: use new ION API.
    The ION API has changed to include a new filed called
    heap_mask in the ion_allocation_data structure. This new field
    now carried the heap bit mask that was previously carried
    by the flags field, which in turn will carry caching and other
    heap information.
    
    Change-Id: I9b296e96fcc051ca07f5e1bf885928eabce8804b

--- QCameraHWI.cpp
+++ QCameraHWI.cpp
@@ -2409,7 +2409,8 @@ int QCameraHardwareInterface::allocate_ion_memory(
   /* to make it page size aligned */
   p_camera_memory->alloc[cnt].len = (p_camera_memory->alloc[cnt].len + 4095) & (~4095);
   p_camera_memory->alloc[cnt].align = 4096;
-  p_camera_memory->alloc[cnt].flags = ion_type;
+  p_camera_memory->alloc[cnt].flags = ION_FLAG_CACHED;
+  p_camera_memory->alloc[cnt].heap_mask = ion_type;
 
   rc = ioctl(p_camera_memory->main_ion_fd[cnt], ION_IOC_ALLOC, &p_camera_memory->alloc[cnt]);
   if (rc < 0) {
@@ -2465,7 +2466,8 @@ int QCameraHardwareInterface::allocate_ion_memory(QCameraStatHeap_t *p_camera_me
   /* to make it page size aligned */
   p_camera_memory->alloc[cnt].len = (p_camera_memory->alloc[cnt].len + 4095) & (~4095);
   p_camera_memory->alloc[cnt].align = 4096;
-  p_camera_memory->alloc[cnt].flags = (0x1 << ion_type | 0x1 << ION_IOMMU_HEAP_ID);
+  p_camera_memory->alloc[cnt].flags = ION_FLAG_CACHED;
+  p_camera_memory->alloc[cnt].heap_mask = (0x1 << ion_type | 0x1 << ION_IOMMU_HEAP_ID);
 
   rc = ioctl(p_camera_memory->main_ion_fd[cnt], ION_IOC_ALLOC, &p_camera_memory->alloc[cnt]);
   if (rc < 0) {
