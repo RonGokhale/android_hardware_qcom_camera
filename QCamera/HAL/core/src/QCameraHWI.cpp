sonyxperiadev/camera

camera: use new ION APIs for cache operations.
This change incorporates the API changes for cache operations.
The cache operations have now been moved to a custom ioctl, with
a new custom_data argument.

Change-Id: I81f0fdb6a7556b433bac6e7ccaa78922aabecdb2

QCamera/HAL/core/src/QCameraHWI.cpp
@@ -2610,8 +2610,10 @@ int QCameraHardwareInterface::cache_ops(int ion_fd,
   struct ion_flush_data *cache_data, int type)
 {
   int rc = 0;
-
-  rc = ioctl(ion_fd, type, cache_data);
+  struct ion_custom_data data;
+  data.cmd = type;
+  data.arg = (unsigned long)cache_data;
+  rc = ioctl(ion_fd, ION_IOC_CUSTOM, &data);
   if (rc < 0)
     ALOGE("%s: Cache Invalidate failed\n", __func__);
   else

RonGokhale/android_hardware_qcom_camera

(cherry picked from commit f320c8c3c058863fa0b2c5da806cf9db7509378e)
Change-Id: I1ddedfca89c061ae424348e5b996b984da51d4e0

QCamera/HAL/core/src/QCameraHWI.cpp
@@ -3276,6 +3276,7 @@ int QCameraHardwareInterface::cache_ops(QCameraHalMemInfo_t *mem_info,
 {
     struct ion_flush_data cache_inv_data;
     int ret = MM_CAMERA_OK;
+    struct ion_custom_data data;
 
 #ifdef USE_ION
     if (NULL == mem_info) {
@@ -3289,12 +3290,15 @@ int QCameraHardwareInterface::cache_ops(QCameraHalMemInfo_t *mem_info,
     cache_inv_data.handle = mem_info->handle;
     cache_inv_data.length = mem_info->size;
 
+    data.cmd = cmd;
+    data.arg = (unsigned long)&cache_inv_data;
+
     ALOGD("addr = %p, fd = %d, handle = %p length = %d, ION Fd = %d",
          cache_inv_data.vaddr, cache_inv_data.fd,
          cache_inv_data.handle, cache_inv_data.length,
          mem_info->main_ion_fd);
     if(mem_info->main_ion_fd > 0) {
-        if(ioctl(mem_info->main_ion_fd,cmd,&cache_inv_data) < 0) {
+        if(ioctl(mem_info->main_ion_fd, ION_IOC_CUSTOM, &data) < 0) {
             ALOGE("%s: Cache Invalidate failed\n", __func__);
             ret = -1;
         }