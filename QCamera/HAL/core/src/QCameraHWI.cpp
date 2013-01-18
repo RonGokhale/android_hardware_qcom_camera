c77b891ce6810be8d53e8cc84d020eac2ffdab0b
Kiran Kumar H N <hurlisal@codeaurora.org>
2013-01-17 17:32:50 -0800

    Camera: HAL: Initialize Exif data before invoking jpeg encode command.
    Initialize the exif data before sending the YUV image for encoding.
    This is to make sure the exif data are properly encoded into the
    final JPEG image.
    
    (cherry picked from commit 59249698b3f672aba27de2d73a6aa523a6328c3e)
    
    Change-Id: I6dab6ab20ce4160a3705cbc9841ce5c252f6ad38
    CRs-Fixed: 433886

--- QCamera/HAL/core/src/QCameraHWI.cpp
+++ QCamera/HAL/core/src/QCameraHWI.cpp
@@ -1,5 +1,5 @@
 /*
-** Copyright (c) 2012 The Linux Foundation. All rights reserved.
+** Copyright (c) 2012-2013 The Linux Foundation. All rights reserved.
 **
 ** Not a Contribution, Apache license notifications and license are retained
 ** for attribution purposes only.
@@ -511,6 +511,9 @@ status_t QCameraHardwareInterface::encodeData(mm_camera_super_buf_t* recvd_frame
     if (thumb_stream) {
         dumpFrameToFile(thumb_frame, HAL_DUMP_FRM_THUMBNAIL);
     }
+
+    //Intialize the Exif data to be passed to the encoder.
+    initExifData();
 
     int jpeg_quality = getJpegQuality();
     if (jpeg_quality <= 0) {
