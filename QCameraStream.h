a2ba8c2e43a4cb165c4fa1e0e1e73cc004ee15d3
Monika Alekhya <malekh@codeaurora.org>
2013-04-19 19:26:35 +0530

    Camera: Update copyright information
    Change-Id: I596ece067d66daaabad68202758bc96c84d26511

--- QCameraStream.h
+++ QCameraStream.h
@@ -1,6 +1,8 @@
 /*
-** Copyright (c) 2009-2013, Code Aurora Forum. All rights reserved.
-**
+** Copyright 2008, Google Inc.
+** Copyright (c) 2009-2013, The Linux Foundation. All rights reserved.
+** Not a Contribution.
+
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
 ** You may obtain a copy of the License at
