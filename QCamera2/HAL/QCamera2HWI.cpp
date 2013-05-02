6912705f333aec1f75f6e151aaa64b12ed1e5299
Dhivya Subramanian <dthiru@codeaurora.org>
2013-05-02 14:20:07 -0700

    Revert "nomerge: Hack to make Video Preview."
    This reverts commit 890dd9edc056e6fd73a775dd358d0604059860ef
    
    Change-Id: I631df60591c72336eecb5fc23d59e2e2b31bc5bb

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -3455,7 +3455,7 @@ int32_t QCamera2HardwareInterface::preparePreview()
     } else {
         bool recordingHint = mParameters.getRecordingHintValue();
         if(recordingHint) {
-            //rc = addChannel(QCAMERA_CH_TYPE_SNAPSHOT);
+            rc = addChannel(QCAMERA_CH_TYPE_SNAPSHOT);
             if (rc != NO_ERROR) {
                 return rc;
             }
