4717d7ca4a97675b3fd1c43a966e99dadefef63a
Vishwanatha Kumar Ulthur <cvulthu@codeaurora.org>
2013-12-19 14:08:13 -0800

    Revert "QCamera2: Add Deffered work thread"
    This reverts commit 2a5e6e53358bfd62731cf847db613bab451db3c9
    
    Change-Id: I09ca7816db2371c9ff404770ab3856f776929ee3
    Conflicts:
    	QCamera2/HAL/QCameraPostProc.cpp
    	QCamera2/HAL/QCameraPostProc.h
    	QCamera2/HAL/QCameraStream.cpp

--- QCamera2/HAL/QCameraPostProc.h
+++ QCamera2/HAL/QCameraPostProc.h
@@ -47,10 +47,6 @@ typedef struct {
     uint32_t client_hdl;             // handle of jpeg client (obtained when open jpeg)
     mm_camera_super_buf_t *src_frame;// source frame (need to be returned back to kernel after done)
     mm_camera_super_buf_t *src_reproc_frame; // original source frame for reproc if not NULL
-    cam_metadata_info_t * metadata;  // source frame metadata
-    bool reproc_frame_release;       // false release original buffer, true don't release it
-    mm_camera_buf_def_t *src_reproc_bufs;
-    mm_camera_buf_def_t *src_bufs;
     QCameraExif *pJpegExifObj;
 } qcamera_jpeg_data_t;
 
@@ -183,7 +179,6 @@ private:
     static const char *STORE_LOCATION;  // path for storing buffers
     bool mUseSaveProc;                  // use store thread
     bool mUseJpegBurst;                 // use jpeg burst encoding mode
-    uint8_t mNewJpegSessionNeeded;
     bool mJpegMemOpt;
 };
 
