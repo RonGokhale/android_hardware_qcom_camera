401e96f724ad1a81aea5f944267c150ab94bbb49
Emilian Peev <epeevs@codeaurora.org>
2014-04-23 18:36:14 -0700

    QCamera2: Supports torch flash mode configuration without preview
    Currently the camera would expect from the client to
    start preview first before trying to enable/disable
    torch mode. This change works around the aforementioned
    limitation by creating a dummy preview channel.
    
    Change-Id: I7197fa65878276954100297bc365d8e4bc1367ac
    CRs-Fixed: 653600

--- QCamera2/HAL/QCamera2HWI.h
+++ QCamera2/HAL/QCamera2HWI.h
@@ -189,9 +189,11 @@ private:
     QCameraCmdThread mProcTh;
     bool             mActive;
 };
+
 class QCamera2HardwareInterface : public QCameraAllocator,
                                   public QCameraThermalCallback,
-                                  public QCameraAdjustFPS
+                                  public QCameraAdjustFPS,
+                                  public QCameraTorchInterface
 {
 public:
     /* static variable and functions accessed by camera service */
@@ -259,6 +261,11 @@ public:
 
     virtual int recalcFPSRange(int &minFPS, int &maxFPS,
             int &vidMinFps, int &vidMaxFps);
+
+    // Implementation of QCameraTorchInterface
+    virtual int prepareTorchCamera();
+    virtual int releaseTorchCamera();
+
     friend class QCameraStateMachine;
     friend class QCameraPostProcessor;
     friend class QCameraCbNotifier;
