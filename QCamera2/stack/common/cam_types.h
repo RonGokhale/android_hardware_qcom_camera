35ad934342117b47ec186747f99f7916d8ad2841
Cullum Baldwin <cullumb@codeaurora.org>
2014-06-25 14:39:53 -0700

    QCamera2: squashed commit from kk_3.5 to kk
    QCamera2: Change focus num steps type
    Id: If56597723873fbc3c8aae64cac20e0635397d397
    
    QCamera2: Add support for AWB mode
    Id: I4d97c97c378d42cd0e75a8d721c530c11479afe9
    
    Change-Id: I75a514b9140c6b495de00146ffed8e1d2b0cb3b4

--- QCamera2/stack/common/cam_types.h
+++ QCamera2/stack/common/cam_types.h
@@ -705,7 +705,7 @@ typedef struct {
 typedef struct {
     uint8_t stepsize;
     uint8_t direction;
-    uint8_t num_steps;
+    int32_t num_steps;
     uint8_t ttype;
 } tune_actuator_t;
 
@@ -1334,6 +1334,7 @@ typedef enum {
     CAM_EZTUNE_CMD_AEC_FORCE_SNAP_LC,
     CAM_EZTUNE_CMD_AEC_FORCE_SNAP_GAIN,
     CAM_EZTUNE_CMD_AEC_FORCE_SNAP_EXP,
+    CAM_EZTUNE_CMD_AWB_MODE,
 } cam_eztune_cmd_type_t;
 
 typedef struct {
@@ -1344,6 +1345,7 @@ typedef struct {
     int                   awb_enable;
     int                   af_enable;
     cam_ez_force_params_t ez_force_param;
+    int                   awb_mode;
   } u;
 } cam_eztune_cmd_data_t;
 
