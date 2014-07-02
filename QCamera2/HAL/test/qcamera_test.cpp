0a0a3d3300506cf6befbdc56e6611f0b7743ebf2
Dimitar Borisov <dboris@codeaurora.org>
2014-07-02 14:35:14 +0300

    camera_test remove recording hint set in startPreview
    The patch removes the recording hint setting when starting the preview.
    Recording hint is already set when configuring the normal/ViV recording.
    
    CRs-Fixed: 688539
    Change-Id: I8890eb4190d7a8e021f36875aa0a0955ce2f90f8

--- QCamera2/HAL/test/qcamera_test.cpp
+++ QCamera2/HAL/test/qcamera_test.cpp
@@ -1816,7 +1816,6 @@ status_t CameraContext::startPreview()
         }
 
         //mParams.set("rdi-mode", "enable");
-        mParams.set("recording-hint", "true");
         mParams.setPreviewSize(previewWidth, previewHeight);
         mParams.setPictureSize(currentPictureSize.width,
             currentPictureSize.height);
