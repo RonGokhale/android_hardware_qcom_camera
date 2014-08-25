7335430c2da59d90eab976c8323aa9fc01614780
Ranjith Kagathi Ananda <ranjith@codeaurora.org>
2014-08-25 02:45:36 -0700

    QCamera2/stack: Add effect and rotation to cam_per_frame_pp_config_t
    Update cam_per_frame_pp_config_t in common headers
    and add cam_rotation_info_t.
    
    Change-Id: Iaaf705fec02f4a07ea75ab9e494b4ba8a5acd114

--- QCamera2/stack/mm-camera-interface/src/cam_intf.c
+++ QCamera2/stack/mm-camera-interface/src/cam_intf.c
@@ -341,6 +341,8 @@ void *get_pointer_of(cam_intf_parm_type_t meta_id,
             return POINTER_OF_META(CAM_INTF_PARM_CAC, metadata);
         case CAM_INTF_PARM_CDS_MODE:
             return POINTER_OF_META(CAM_INTF_PARM_CDS_MODE, metadata);
+        case CAM_INTF_PARM_ROTATION:
+          return POINTER_OF_META(CAM_INTF_PARM_ROTATION, metadata);
         default:
             return NULL;
     }
@@ -658,6 +660,8 @@ uint32_t get_size_of(cam_intf_parm_type_t param_id)
             return SIZE_OF_PARAM(CAM_INTF_PARM_CAC, metadata);
         case CAM_INTF_PARM_CDS_MODE:
             return SIZE_OF_PARAM(CAM_INTF_PARM_CDS_MODE, metadata);
+        case CAM_INTF_PARM_ROTATION:
+          return SIZE_OF_PARAM(CAM_INTF_PARM_ROTATION, metadata);
         default:
             return 0;
     }
