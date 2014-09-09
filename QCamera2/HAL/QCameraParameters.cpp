bfc8f3f361ab8f4b7ef9ebebd1f1320dcdea0632
Dimitar Borisov <dboris@codeaurora.org>
2014-09-09 16:37:23 +0300

    QCamera2: Check if CDS mode is supported before set it
    - Checking whether CDS mode is supported has been
      already added in kk_1 with commit 2e0c5e26.
    - This commit expands visibility of a heap allocated.
      data 'prop' and adds additional debug error message
    - This commit is just syncing code with the master branch
    
    CRs-Fixed: 715042
    Change-Id: Ic8fbf0b55b61a68d91da1dad12d99a983371790f

--- QCamera2/HAL/QCameraParameters.cpp
+++ QCamera2/HAL/QCameraParameters.cpp
@@ -5693,13 +5693,13 @@ int32_t QCameraParameters::setCDSMode(const QCameraParameters& params)
     const char *prev_str = get(KEY_QC_CDS_MODE);
     char *cds_mode_str = NULL;
     int32_t rc = NO_ERROR;
+    char prop[PROPERTY_VALUE_MAX];
 
     if (str) {
         if (!prev_str || !strcmp(str, prev_str)) {
             cds_mode_str = (char *)str;
         }
     } else {
-        char prop[PROPERTY_VALUE_MAX];
         memset(prop, 0, sizeof(prop));
         property_get("persist.camera.CDS", prop, CDS_MODE_AUTO);
         cds_mode_str = prop;
@@ -5714,6 +5714,9 @@ int32_t QCameraParameters::setCDSMode(const QCameraParameters& params)
             if (rc != NO_ERROR) {
                 ALOGE("%s:Failed CDS MODE to update table", __func__);
             }
+        } else {
+            ALOGE("%s: Invalid argument for CDS MODE %s", __func__,  cds_mode_str);
+            rc = BAD_VALUE;
         }
     }
     return rc;
