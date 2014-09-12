60831c92c57105e387cde05f7ed08d335b1459ce
Krupal Divvela <kdivvela@codeaurora.org>
2014-09-12 18:42:51 +0530

    QCamera2: Fix multi touch issues
    Fix buffer allocation issue and crop enable
    issue during multi touch.
    
    Change-Id: Ib5e2807d26ef7ce5de24ee45d720f816b55cd09e
    Signed-off-by: Sucharitha Chadalawada <schadala@codeaurora.org>

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -3057,6 +3057,12 @@ int QCamera2HardwareInterface::cancelPicture()
     if (mParameters.isUbiFocusEnabled()) {
         configureAFBracketing(false);
     }
+    if (mParameters.isMultiTouchFocusEnabled()) {
+        configureMTFBracketing(false);
+    }
+    if (mParameters.isMultiTouchFocusSelected()) {
+        mParameters.resetMultiTouchFocusParam();
+    }
     return NO_ERROR;
 }
 
@@ -3080,12 +3086,6 @@ void QCamera2HardwareInterface::captureDone()
     if (mParameters.isfssrEnabled()) {
         CDBG_HIGH("%s: Restoring previous zoom value!!",__func__);
         mParameters.setAndCommitZoom(mZoomLevel);
-    }
-    if (mParameters.isMultiTouchFocusEnabled()) {
-        configureMTFBracketing(false);
-    }
-    if (mParameters.isMultiTouchFocusSelected()) {
-        mParameters.resetMultiTouchFocusParam();
     }
 }
 
