385e92b819283864814143500958842298b2525d
Vijay kumar Tumati <vtumati@codeaurora.org>
2014-11-12 11:58:10 +0530

    QCamera2: Add param lock in getParameters
    Not using param lock in getParameters leads
    to concurrent access of the parameters memory
    and a crash subsequently.
    
    Change-Id: Ib380d587db510461c1bd85a30cdedfd677046771

--- QCamera2/HAL/QCamera2HWI.cpp
+++ QCamera2/HAL/QCamera2HWI.cpp
@@ -3039,7 +3039,7 @@ char* QCamera2HardwareInterface::getParameters()
     String8 str;
 
     int cur_width, cur_height;
-
+    pthread_mutex_lock(&m_parm_lock);
     //Need take care Scale picture size
     if(mParameters.m_reprocScaleParam.isScaleEnabled() &&
         mParameters.m_reprocScaleParam.isUnderScaling()){
@@ -3072,6 +3072,7 @@ char* QCamera2HardwareInterface::getParameters()
         pic_size.append(buffer);
         mParameters.set(CameraParameters::KEY_PICTURE_SIZE, pic_size);
     }
+    pthread_mutex_unlock(&m_parm_lock);
     return strParams;
 }
 
