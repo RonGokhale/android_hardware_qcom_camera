15da7f0ff9aa6b18e587482935a2076d6f30b4d5
Sujai SyrilRaj <ssyrilra@codeaurora.org>
2014-11-28 10:12:14 -0800

    camera-HAL: Moving camera HAL projects to LNX.LA.3.5.2.2
    changes to move HAL projects based on LNX.LA.3.5.2.2
    to Android L release
    
    Change-Id: Id8c26e2104766fc74c0dda49ead7a46b21b4e684

--- QCamera2/HAL/QCameraParameters.cpp
+++ QCamera2/HAL/QCameraParameters.cpp
@@ -6843,7 +6843,6 @@ int32_t QCameraParameters::enableFlash(bool enableFlash, bool commitSettings)
         str);
 
     if (value != NAME_NOT_FOUND) {
-        ALOGV("%s: Setting Flash value %s", __func__, flashStr);
 
         rc = AddSetParmEntryToBatch(m_pParamBuf,
                                       CAM_INTF_PARM_LED_MODE,
