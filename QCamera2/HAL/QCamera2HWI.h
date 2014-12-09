27fa56e9af0ce6a26680bfe8a1f11a3b30848b28
Vladimir Petrov <vpetro@codeaurora.org>
2014-12-09 17:58:40 +0200

    QCamera2: Reject API calls after SM thread release
    - Camera client does concurrent calls to camera
      hardware. It is possible to call another API
      while calling disconnect(). In this case
      disconnect() call may release HAL state machine
      thread, causing other call to be blocked awaiting
      completion.
    - This patch avoids deadlocks by rejecting all
      API calls after releasing SM thread.
    
    Change-Id: I589f40114e6455d9d016f81439a5a82aef1be358

--- QCamera2/HAL/QCamera2HWI.h
+++ QCamera2/HAL/QCamera2HWI.h
@@ -542,6 +542,7 @@ private:
     void                          *mCallbackCookie;
 
     QCameraStateMachine m_stateMachine;   // state machine
+    bool m_smThreadActive;
     QCameraPostProcessor m_postprocessor; // post processor
     QCameraThermalAdapter &m_thermalAdapter;
     QCameraCbNotifier m_cbNotifier;
