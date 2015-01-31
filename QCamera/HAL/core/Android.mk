c6435dc71c5eed5970eb45618654b89d335c28fa
RonGokhale <cips173@gmail.com>
2015-01-31 09:42:19 +0530

    QCamera: HAL: use project pathmap
    Change-Id: Ib47c61658dc357935dca74f776dbb7dcb42083cb

--- QCamera/HAL/core/Android.mk
+++ QCamera/HAL/core/Android.mk
@@ -75,8 +75,8 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
-LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc \
-        hardware/qcom/media/libstagefrighthw \
+LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc \
+        $(call project-path-for,qcom-media)/libstagefrighthw \
         system/media/camera/include
 
 # if debug service layer and up , use stub camera!
